3105136d10673d4c9fe80ceee5e68d082dd0fd85
peijialu <peijialu@usc.edu>
2018-02-26 00:35:26 -0800

    correct pic position

---
+++ collageBuilderMockup/src/collageBuilderMockup/APIResponse.java
@@ -0,0 +1,16 @@
+package collageBuilderMockup;
+
+import java.util.List;
+
+public class APIResponse {
+    SearchInfo searchInformation;
+    Item[] items;
+
+    public SearchInfo getSearchInformation() {
+        return searchInformation;
+    }
+
+    public Item[] getItems() {
+        return items;
+    }
+}
---
+++ collageBuilderMockup/src/collageBuilderMockup/SearchInfo.java
@@ -0,0 +1,14 @@
+package collageBuilderMockup;
+
+public class SearchInfo {
+    double searchTime;
+    String formattedSearchTime;
+
+    public double getSearchTime() {
+        return searchTime;
+    }
+
+    public String getFormattedSearchTime() {
+        return formattedSearchTime;
+    }
+}
